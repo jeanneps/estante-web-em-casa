fc87b0992eebc9daaed4f6fcbcf5fd669fefa5e7
jeanneps <jeanepacchecosoares@gmail.com>
2024-08-30 14:28:30 -0300

    feito pagina exp_livro

--- index.php
+++ index.php
@@ -3,21 +3,17 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/jeane/estante_web/views/_cabecalho.ph
 require_once $_SERVER['DOCUMENT_ROOT'] . '/jeane/estante_web/models/livros.php';
 
 $livros = livros::mostrarLivros();
-
 ?>
 
 <main id="index">
     <div id="livros_conteiner">
         <?php foreach ($livros as $livro): ?>
-            <a href="/jeane/estante_web/views/exp_livro.php?id=<?= htmlspecialchars($livro['id']) ?>">
-
-            <img id="capa" src="data:image/jpeg;base64, <?php echo base64_encode($livro['capa']); ?>" alt="capa de livro">
-            <h2 id="titulo"><?php echo htmlspecialchars($livro['titulo']); ?></h2>
+            <a href="/jeane/estante_web/views/exp_livro.php?id=<?= htmlspecialchars($livro['id_livro']) ?>">
+                <img id="capa" src="data:image/jpeg;base64, <?= base64_encode($livro['capa']); ?>" alt="capa de livro">
+                <h2 id="titulo"><?= htmlspecialchars($livro['titulo']); ?></h2>
             </a>
             <img src="/jeane/estante_web/imgs/favorito.svg" alt="">
-            </div>
         <?php endforeach; ?>
     </div>
-    <?php
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/jeane/estante_web/views/_rodape.php';
-    ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/jeane/estante_web/views/_rodape.php'; ?>
+</main>
